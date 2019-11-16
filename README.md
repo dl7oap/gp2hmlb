75986a6ad7d1c231bb0e86dc74d3354a094c143b
Andreas Puschendorf <andreas@puschendorf.net>
2019-11-16 18:58:47 +0100

    add 23cm support and more   - added 23cm support   - improve readme   - set SQL and AF   - when FM set AFC ON for downlink

--- README.md
+++ README.md
@@ -2,67 +2,63 @@ Author  : Andreas Puschendorf, DL7OAP
 Date    : 2019-11-15
 
 
-General 
-#######
+# General
 
 This script is plugged between gpredict and hamlib based on python 3.7.
 It is listing on port 4532 for gpredict frequencies
 and it is sending frequencies and startsequences for ic9700 to port 4572 for hamlib
 
-the main reason for this plugin is to solve https://github.com/csete/gpredict/issues/181 
-so i can use ICOM ic9700 with gepredict and linux. the script is a alphaversion and should 
+The main reason for this plugin is to solve https://github.com/csete/gpredict/issues/181
+so i can use ICOM ic9700 with gepredict and linux. The script is a alphaversion and should
 help to get a better understanding how the startsequence and the loop have to be develop
-so gpredict can be fixed for ic9700. it is also a testplattform to change fast the 
+so gpredict can be fixed for ic9700. It is also a testplattform to change fast the
 used hamlib commands to play around with the behavior of the ic9700 and hamlib.
 
 And it should not fix only the problem with ic9700 but also with ic9100 and ic910.
 https://github.com/csete/gpredict/issues/42
-so reports for ic9100 and ic910 are also welcome.
+So reports for ic9100 and ic910 are also welcome.
 
 
-Detail
-######
+# Usage details
+
+gp2hmlb.py hast to be called with 2 parameter
 
 Usage: python3 gp2hmlb.py [type_of_satellite] [band_of_uplink]
 
-gp2hmlb.py hast to be called with 2 parameter
-first parameter is type of satellite. you can choose between FM, SSB, SIMPLEX, CW
-second parameter is type of uplink. you can choose between 2M, 70CM
+1. Parameter is type of satellite. You can choose between FM, SSB, SIMPLEX, CW
+2. Parameter is type of uplink. You can choose between 2M, 70CM
 
 
-Hints
-#####
-for SIMPLEX and FM satellits i use an update rate of 10000ms in gpredict. this is enough.
-you can turn on the AFC function of ic9700, this can also eleminate a lot of the doppler effect.
+# Hints
 
-for SSB/CW i use an update rate between 800ms and 2000ms. so cw signals will be ok with 2000ms. 
-when you using the main dail to change the frequency 2000ms feels a little bit long. because you have
+For SIMPLEX and FM satellites i use an update rate of 10000ms in gpredict. This is more then enough.
+You can turn on the AFC function manually on the downlink of ic9700, this can eleminate a lot of the doppler effect.
+
+For SSB/CW i use an update rate between 800ms and 2000ms. So cw signals will be ok with 2000ms.
+When you using the main dail to change the frequency 2000ms feels a little bit long. Because you have
 to wait until gepredict have catch the new downlink frequency and the new matching update frequency is
-send to ic9700. you have to play around with this :)
+send to ic9700. You have to play around with this :)
 
-this script always starts the uplink in LSB and the downlik in USB. i think most of the common satellites
-can be worked with this, but when not we have to enhance the parameter set.
-
-
-Requirements
-############
-
-- gepredict version 2.3.* (older should also possible)
-- hamlib 3.3 (i'm using 4.0 daily snapshot of 9th Nov 2019 http://n0nb.users.sourceforge.net)
-- python 3.7 (python 2.* should not work correct)
+This script always starts the uplink in LSB and the downlik in USB. Most of the common satellites
+can be worked with this, but when not, we have to enhance the parameter set.
 
 
-Starting
-########
+# Requirements
 
-1t step:    starting hamlib daemon on port 4572
-    Linux:      rigctld -m 381 -r /dev/ic9700a -s 19200 -t 4572
-    Windows:    rigctld -m 381 -r COM5 -s 19200 -t 4572
-2t step:    starting gp2hmlb.py pythonscript
-    Linux:      python3 gp2hmlb.py FM 70CM
-    Windows:    python3 gp2hmlb.py FM 70CM
-3t step:    start gepredict with a duplex trx on port 4532 and MAIN/SUB
-'''gp2hmlib.py is a plugin between gpredict and hamlib to use and test ic9700 with gpredict
+* gepredict version 2.3.* (older should also possible)
+* hamlib 3.3 (i'm using 4.0 daily snapshot of 9th Nov 2019 http://n0nb.users.sourceforge.net)
+* python 3.7 (python 2.* should not work correct)
+
+
+# Starting order
+
+1. starting hamlib daemon on port 4572
+    * Linux:      rigctld -m 381 -r /dev/ic9700a -s 19200 -t 4572
+    * Windows:    rigctld -m 381 -r COM5 -s 19200 -t 4572
+2. starting gp2hmlb.py pythonscript
+    * Linux:      python3 gp2hmlb.py FM 70CM
+    * Windows:    python3 gp2hmlb.py FM 70CM
+3. start gepredict with a duplex trx on port 4532 and MAIN/SUB
 
 
 
--- gp2hmlb.py
+++ gp2hmlb.py
@@ -2,8 +2,8 @@
 
 '''
 Author  : Andreas Puschendorf, DL7OAP
-Version : 003
-Date    : 2019-11-15
+Version : 004
+Date    : 2019-11-16
 
 This script is plugged between gpredict and hamlib based on python 3.7
 It is listing on port 4532 for gpredict frequencies
@@ -11,8 +11,8 @@ and it is sending frequencies and startsequences for ic9700 to port 4572 for ham
 
 Usage: python3 gp2hmlb.py [type_of_satellite] [band_of_uplink]
 gp2hmlb.py hast to be called with 2 parameter
-first parameter is type of satellite. you can choose betwween FM, SSB, SIMPLEX, CW
-second parameter is type of uplink. you can choose between 2M, 70CM
+first parameter is type of satellite. you can choose between FM, SSB, SIMPLEX, CW
+second parameter is type of uplink. You can choose between 2M, 70CM, 23CM
 
 1t step:    starting hamlib daemon on port 4572
     Linux:      rigctld -m 381 -r /dev/ic9700a -s 19200 -t 4572
@@ -22,6 +22,12 @@ second parameter is type of uplink. you can choose between 2M, 70CM
     Windows:    python3 gp2hmlb.py FM 70CM
 3t step:    start gepredict with a duplex trx on port 4532 and MAIN/SUB
 '''
+
+
+# TODO: when FM, then enable AFC on downlink, when not FM disable AFC (will be U AFC 1/0)
+# TODO: when possible, disable in general start sequence DUP (hamlib command is missing)
+#       fe fe a2 00 0F 10 fd  set DUP off (R None)
+# TODO: are there satellites with USB uplink? when yes we have to split SSB parameter into LSB and USB
 
 
 import socket
@@ -40,7 +46,21 @@ def sendCommandToHamlib(sock_hamlib, command):
     return sock_hamlib.recv(100).decode('utf-8')
 
 
-def startSequenceSSB(sock_hamlib):
+def setStartSequenceGeneral(sock_hamlib):
+
+    # define uplink
+    sendCommandToHamlib(sock_hamlib, b'V Main\n')
+    sendCommandToHamlib(sock_hamlib, b'L SQL 0\n')
+    sendCommandToHamlib(sock_hamlib, b'L AF 0\n')
+    #sendCommandToHamlib(sock_hamlib, b'R None\n')
+
+    # define downlink
+    sendCommandToHamlib(sock_hamlib, b'V Sub\n')
+    sendCommandToHamlib(sock_hamlib, b'L SQL 0\n')
+    sendCommandToHamlib(sock_hamlib, b'L AF 0.15\n')
+
+
+def setStartSequenceSSB(sock_hamlib):
 
     # define uplink
     sendCommandToHamlib(sock_hamlib, b'V Main\n')
@@ -54,7 +74,7 @@ def startSequenceSSB(sock_hamlib):
     sendCommandToHamlib(sock_hamlib, b'M USB 2400\n')
 
 
-def startSequenceCW(sock_hamlib):
+def setStartSequenceCW(sock_hamlib):
 
     # define uplink
     sendCommandToHamlib(sock_hamlib, b'V Main\n')
@@ -65,16 +85,17 @@ def startSequenceCW(sock_hamlib):
     # define downlink
     sendCommandToHamlib(sock_hamlib, b'V Sub\n')
     sendCommandToHamlib(sock_hamlib, b'V VFOA\n')
-    sendCommandToHamlib(sock_hamlib, b'M CW 500\n')
+    sendCommandToHamlib(sock_hamlib, b'M USB 2400\n')
 
 
-def startSequenceFM(sock_hamlib):
+def setStartSequenceFM(sock_hamlib):
 
     # define uplink
     sendCommandToHamlib(sock_hamlib, b'V Main\n')
     sendCommandToHamlib(sock_hamlib, b'V VFOA\n')
     sendCommandToHamlib(sock_hamlib, b'M FM 15000\n')
     sendCommandToHamlib(sock_hamlib, b'S 0 Main\n')
+    sendCommandToHamlib(sock_hamlib, b'U AFC 0\n')
 
     sendCommandToHamlib(sock_hamlib, b'C 670\n')
     sendCommandToHamlib(sock_hamlib, b'U TONE 1\n')
@@ -84,21 +105,24 @@ def startSequenceFM(sock_hamlib):
     sendCommandToHamlib(sock_hamlib, b'V VFOA\n')
     sendCommandToHamlib(sock_hamlib, b'M FM 15000\n')
     sendCommandToHamlib(sock_hamlib, b'U TONE 0\n')
+    sendCommandToHamlib(sock_hamlib, b'U AFC 1\n')
 
 
-def startSequenceSimplex(sock_hamlib):
+def setStartSequenceSimplex(sock_hamlib):
 
     # define uplink
     sendCommandToHamlib(sock_hamlib, b'V Main\n')
     sendCommandToHamlib(sock_hamlib, b'V VFOB\n')
     sendCommandToHamlib(sock_hamlib, b'M FM 15000\n')
     sendCommandToHamlib(sock_hamlib, b'S 0 Main\n')
+    sendCommandToHamlib(sock_hamlib, b'U AFC 0\n')
 
     # define downlink
     sendCommandToHamlib(sock_hamlib, b'V VFOA\n')
     sendCommandToHamlib(sock_hamlib, b'M FM 15000\n')
     sendCommandToHamlib(sock_hamlib, b'U TONE 0\n')
     sendCommandToHamlib(sock_hamlib, b'S 1 Main\n')
+    sendCommandToHamlib(sock_hamlib, b'U AFC 1\n')
 
 
 def loopSSBandFMandCW(sock_hamlib, up, dw):
@@ -134,13 +158,41 @@ def loopSIMPLEX(sock_hamlib, up, dw):
         sendCommandToHamlib(sock_hamlib, b)
 
 
+def getBandFromFrequency(frequency):
+    band = '2M'
+    if int(frequency) > 150000000:
+        band = '70CM'
+    if int(frequency) > 470000000:
+        band = '23CM'
+    return band
+
+
+def activateCorrectUplinkBandInMain(sock_hamlib, type_of_uplink_band):
+    sendCommandToHamlib(sock_hamlib, b'V Main\n')
+    main_frequency = sendCommandToHamlib(sock_hamlib, b'f\n')
+    sendCommandToHamlib(sock_hamlib, b'V Sub\n')
+    sub_frequency = sendCommandToHamlib(sock_hamlib, b'f\n')
+
+    if type_of_uplink_band == getBandFromFrequency(main_frequency):  # is uplink band in main -> nothing to do
+        return
+    elif type_of_uplink_band == getBandFromFrequency(sub_frequency):  # is uplink band in sub -> switch bands
+        sendCommandToHamlib(sock_hamlib, b'G XCHG\n')
+    else:  # is uplink band not in main and sub -> set band in main
+        sendCommandToHamlib(sock_hamlib, b'V Main\n')
+        if type_of_uplink_band == '23CM':
+            sendCommandToHamlib(sock_hamlib, b'F 1295000000\n')
+        if type_of_uplink_band == '70CM':
+            sendCommandToHamlib(sock_hamlib, b'F 435000000\n')
+        if type_of_uplink_band == '2M':
+            sendCommandToHamlib(sock_hamlib, b'F 145900000\n')
+
+
 def main():
 
     uplink = '0'
     downlink = '0'
     last_uplink = '0'
     last_downlink = '0'
-    type_of_satellite = 'FM'
 
     ###############################################
     # start sockets for gpredict und hamlib
@@ -168,28 +220,25 @@ def main():
         type_of_satellite = sys.argv[1].upper()
         type_of_uplink_band = sys.argv[2].upper()
 
-        if type_of_uplink_band not in ['70CM','2M']:
-            print('ERROR: unkown uplink band type "' + type_of_uplink_band + '". Only 2M and 70CM possible.')
+        if type_of_uplink_band not in ['70CM', '2M', '23CM']:
+            print('ERROR: unknown uplink band type "' + type_of_uplink_band + '". Only 2M, 70CM and 23CM possible.')
             return
 
-        # exchange 2m / 70cm when wrong band is active in main for uplink
-        sendCommandToHamlib(sock_hamlib, b'V Main\n')
-        actual_main_frequency = sendCommandToHamlib(sock_hamlib, b'f\n')
-        if ((type_of_uplink_band == '70CM') and (actual_main_frequency[0:2] == '14'))  \
-                or ((type_of_uplink_band == '2M') and (actual_main_frequency[0:1] == '4')):
-            sendCommandToHamlib(sock_hamlib, b'G XCHG\n')
+        activateCorrectUplinkBandInMain(sock_hamlib, type_of_uplink_band)
 
         if type_of_satellite == 'SSB':
-            startSequenceSSB(sock_hamlib)
+            setStartSequenceSSB(sock_hamlib)
         elif type_of_satellite == 'CW':
-            startSequenceCW(sock_hamlib)
+            setStartSequenceCW(sock_hamlib)
         elif type_of_satellite == 'FM':
-            startSequenceFM(sock_hamlib)
+            setStartSequenceFM(sock_hamlib)
         elif type_of_satellite == 'SIMPLEX':
-            startSequenceSimplex(sock_hamlib)
+            setStartSequenceSimplex(sock_hamlib)
         else:
             print('ERROR: unkown satellite type "' + type_of_satellite + '". Only SSB, FM, SIMPLEX or CW possible.')
             return
+        setStartSequenceGeneral(sock_hamlib)
+
     else:
         print('Usage: gp2hmlb.py [type_of_satellite] [band_of_uplink]')
         print('INFO: gp2hmlb.py hast to be called with 2 parameter')
@@ -213,9 +262,9 @@ def main():
                 # get downlink and uplink from gpredict
                 cut = data.decode('utf-8').split(' ')
                 if data[0] == 70:  # F
-                    downlink = cut[2].replace('\n', '')
+                    downlink = cut[len(cut)-1].replace('\n', '')
                 if data[0] == 73:  # I
-                    uplink = cut[2].replace('\n', '')
+                    uplink = cut[len(cut)-1].replace('\n', '')
                 print('last  = ' + last_uplink + ' / ' + last_downlink)
                 print('fresh = ' + uplink + ' / ' + downlink)
                 # only if uplink or downlink changed >0 10Hz Column, then update
